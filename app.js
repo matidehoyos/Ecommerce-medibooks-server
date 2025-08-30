37e9523a85fa9044339da0904089cf54745e52dd
matidehoyos <mati.dehoyosmdp@gmail.com>
2025-08-30 10:17:05 -0300

    fix: sync

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-sequelize.sync()
+sequelize.sync({ alter: true })
   .then(() => {
       console.log('Base de datos sincronizada');
       app.listen(PORT, () => {
